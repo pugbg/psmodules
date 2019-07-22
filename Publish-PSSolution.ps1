7a7f1f353a1dbcee0c96340c1c5461de03eff5bc
Ivanov <georgimilchevivanov@kpmg.com>
2019-07-22 17:30:01 +0300

    fix publish script

--- Publish-PSSolution.ps1
+++ Publish-PSSolution.ps1
@@ -6,13 +6,26 @@ param
 
 process
 {
-	$ModulesFolder = "$PSScriptRoot\modules"
+	#Import RequiredModules from this project
+	try
+	{
+		Write-Information "Import RequiredModules from this project started" -InformationAction Continue
 
-	Import-Module -FullyQualifiedName "$ModulesFolder\AstExtensions" -Force -ErrorAction Stop
-	Import-Module -FullyQualifiedName "$ModulesFolder\TypeHelper" -Force -ErrorAction Stop
-	Import-Module -FullyQualifiedName "$ModulesFolder\PSHelper" -Force -ErrorAction Stop
-	Import-Module -FullyQualifiedName "$ModulesFolder\PSBuild" -Force -ErrorAction Stop
-	
+		$ModulesFolder = "$PSScriptRoot\modules"
+
+		Import-Module -FullyQualifiedName "$ModulesFolder\AstExtensions" -Force -ErrorAction Stop
+		Import-Module -FullyQualifiedName "$ModulesFolder\SystemExtensions" -Force -ErrorAction Stop
+		Import-Module -FullyQualifiedName "$ModulesFolder\TypeHelper" -Force -ErrorAction Stop
+		Import-Module -FullyQualifiedName "$ModulesFolder\PSHelper" -Force -ErrorAction Stop
+		Import-Module -FullyQualifiedName "$ModulesFolder\PSBuild" -Force -ErrorAction Stop
+
+		Write-Information "Import RequiredModules from this project completed" -InformationAction Continue
+	}
+	catch
+	{
+		throw "Import RequiredModules from this project failed. Details: $_"
+	}
+		
 	$SolutionConfiguration = Get-PSSolutionConfiguration -Path 'buildconfig.psd1' -UserVariables @{
 		PSGalleryApiKey=$PSGalleryApiKey
 	}
