196e0dc43b6f3a09e17976bb07ff5da03cda02a2
Ivanov <georgimilchevivanov@kpmg.com>
2019-07-25 11:33:46 +0300

    Added New-AzeKeyVault,Remove-AzeKeyVault

--- modules/AzExtensions/tests/AzExtensions.tests.ps1
+++ modules/AzExtensions/tests/AzExtensions.tests.ps1
@@ -1,3 +1,10 @@
+$TestEnvironment = @{
+    TenantId          = '098ebab6-0ca3-4735-973c-7e8b14e101ac'
+    SubscriptionId    = 'a65a9513-cde5-4852-8b5d-4f50d3c43c58'
+    ApplicationId     = '5dfc81fc-ebbd-44de-a7af-ffa730d87b62'
+    ApplicationSecret = Get-AzKeyVaultSecret -VaultName 'psmodules-cicd' -Name 'psmodules-cicd' | select -ExpandProperty SecretValueText
+}
+
 describe 'AzeOAuthToken' {
 
     context "Using User" {
@@ -53,14 +60,10 @@ describe 'ResourceGroup' {
 
     context "Create, Get, Remove using '-oAuthToken' parameter" {
 
-        $TestContext = @{
-            SubscriptionId     = 'a65a9513-cde5-4852-8b5d-4f50d3c43c58'
-            TenantId           = '098ebab6-0ca3-4735-973c-7e8b14e101ac'
-            ResourceGroupName  = 'test-AzeModuleGrp01'
-            Location           = 'northeurope'
-            oAuthToken         = $null
-            ApplicationId     = 'c7c60ad5-3f5a-417e-aadc-98ad3d95edc5'
-            ApplicationSecret = $Env:ApplicationSecret
+        $TestContext = $TestEnvironment + @{
+            ResourceGroupName = 'test-AzeModuleGrp01'
+            Location          = 'northeurope'
+            oAuthToken        = $null
         }
 
         it "Initialize" {
@@ -82,21 +85,15 @@ describe 'ResourceGroup' {
 
         it "Remove using '-oAuthToken' parameter" {
             Remove-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -oAuthToken $TestContext['oAuthToken']
-            {Get-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -oAuthToken $TestContext['oAuthToken'] -ErrorAction Stop } | should -Throw
+            { Get-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -oAuthToken $TestContext['oAuthToken'] -ErrorAction Stop } | should -Throw
         }
-
     }
 
     context "Create, Get, Remove not using '-oAuthToken' parameter" {
 
-        $TestContext = @{
-            SubscriptionId     = 'a65a9513-cde5-4852-8b5d-4f50d3c43c58'
-            TenantId           = '098ebab6-0ca3-4735-973c-7e8b14e101ac'
-            ResourceGroupName  = 'test-AzeModuleGrp02'
-            Location           = 'northeurope'
-            oAuthToken         = $null
-            ApplicationId     = 'c7c60ad5-3f5a-417e-aadc-98ad3d95edc5'
-            ApplicationSecret = $Env:ApplicationSecret
+        $TestContext = $TestEnvironment + @{
+            ResourceGroupName = 'test-AzeModuleGrp02'
+            Location          = 'northeurope'
         }
 
         it "Initialize" {
@@ -118,7 +115,7 @@ describe 'ResourceGroup' {
 
         it "Remove not using '-oAuthToken' parameter" {
             Remove-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName']
-            {Get-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -ErrorAction Stop } | should -Throw
+            { Get-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -ErrorAction Stop } | should -Throw
         }
 
     }
@@ -139,15 +136,11 @@ describe 'RoleAssignment' {
     }
 
     context "Create not using '-oAuthToken' parameter" {
-        $TestContext = @{
-            SubscriptionId     = 'a65a9513-cde5-4852-8b5d-4f50d3c43c58'
-            TenantId           = '098ebab6-0ca3-4735-973c-7e8b14e101ac'
-            ResourceGroupName  = 'test-AzeModuleGrp03'
-            Location           = 'northeurope'
-            oAuthToken         = $null
-            ApplicationId     = 'c7c60ad5-3f5a-417e-aadc-98ad3d95edc5'
-            ApplicationSecret = $Env:ApplicationSecret
-            PrincipalId = '065920d4-336e-44e3-a44a-3b18aaf85041'
+
+        $TestContext = $TestEnvironment + @{
+            ResourceGroupName = 'test-AzeModuleGrp03'
+            Location          = 'northeurope'
+            PrincipalId       = '065920d4-336e-44e3-a44a-3b18aaf85041'
         }
 
         it "Initialize" {
@@ -165,6 +158,91 @@ describe 'RoleAssignment' {
         it "Create on Subscription scope" {
             $RoleDefinition = Get-AzeRoleDefinition -Name 'Contributor' -Scope "subscriptions/$($TestContext['SubscriptionId'])"
             $RoleAssignment = New-AzeRoleAssignment -PrincipalId $TestContext['PrincipalId'] -Scope "subscriptions/$($TestContext['SubscriptionId'])" -RoleDefinitionId $RoleDefinition.Id -PassThru
+        }
+
+        it "Cleanup" {
+            $RG = Get-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -ErrorAction SilentlyContinue
+            if ($RG)
+            {
+                Remove-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -ErrorAction Stop
+            }
+        }
+    }
+}
+
+describe 'KeyVault' {
+  
+    BeforeEach {
+
+        #Configure Context to skip processing 'it` statements on failed 'it' statement
+        $CurrentTestGroup = InModuleScope -ModuleName Pester { $Pester.CurrentTestGroup }
+        if (($CurrentTestGroup.Actions.Passed -contains $false) -and ((InModuleScope -ModuleName Pester { $Name }) -ne 'Cleanup'))
+        {
+            Set-ItResult -Skipped -Because 'previous test failed'
+        }
+
+    }
+
+    context "Create, Remove not using '-oAuthToken' parameter" {
+
+        $TestContext = $TestEnvironment + @{
+            ResourceGroupName = 'test-AzeModuleGrp04'
+            Location          = 'northeurope'
+            KeyVaultName      = 'test-aze-keyvault-01'
+            properties        = @{
+                TenantId = $TestEnvironment['TenantId']
+                sku      = @{
+                    family = 'A'
+                    name   = 'standard'
+                }
+                accessPolicies=@()
+            }
+            Tags              = @{
+                Tag1 = 'Value1'
+            }
+        }
+
+        it "Initialize" {
+            $ModuleRoot = Split-Path -Path $PSScriptRoot -Parent
+            Import-Module -FullyQualifiedName $ModuleRoot -Force -ErrorAction Stop
+            Connect-AzeAccount -TenantId $TestContext['TenantId'] -ApplicationId $TestContext['ApplicationId'] -ApplicationSecret $TestContext['ApplicationSecret']
+            New-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -Location $TestContext['Location']
+        }
+
+        it "Create" {
+            $NewAzeKeyVault_Params = @{
+                SubscriptionId    = $TestContext['SubscriptionId']
+                ResourceGroupName = $TestContext['ResourceGroupName']
+                Name              = $TestContext['KeyVaultName']
+                Location          = $TestContext['Location']
+                Tags              = $TestContext['Tags']
+                PassThru          = $true
+                Properties        = $TestContext['Properties']
+            }
+            $KeyVault = New-AzeKeyVault @NewAzeKeyVault_Params -ErrorAction Stop
+            $KeyVault.Name | should -be $TestContext['KeyVaultName']
+            $KeyVault.Location | should -be $TestContext['Location']
+            $TestContext['Tags'].Keys | foreach {
+                $KeyVault.Tags."$_" | should -be $TestContext['Tags']["$_"]
+            }
+        }
+
+        it "Remove" {
+            $RemoveAzeKeyVault_Params = @{
+                SubscriptionId    = $TestContext['SubscriptionId']
+                ResourceGroupName = $TestContext['ResourceGroupName']
+                Name              = $TestContext['KeyVaultName']
+            }
+            $KeyVault = Remove-AzeKeyVault @RemoveAzeKeyVault_Params -ErrorAction Stop
+            $keyvault | should -BeNullOrEmpty
+        }
+
+        it "Cleanup" {
+            $RG = Get-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -ErrorAction SilentlyContinue
+            if ($RG)
+            {
+                Remove-AzeResourceGroup -SubscriptionId $TestContext['SubscriptionId'] -Name $TestContext['ResourceGroupName'] -ErrorAction Stop
+            }
         }
     }
 }
